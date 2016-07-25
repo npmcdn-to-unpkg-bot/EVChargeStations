a9766dc400a310c0de4417b2b8466e58105b8ed8
Daniel-Jay Pascual <dpascual@Daniel-Jays-MacBook-Pro-2.local>
2016-07-25 13:39:27 -1000

    USING GEOLOCATION CORDOVA PLUGIN

--- www/js/app.js
+++ www/js/app.js
@@ -77,5 +77,5 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/map');
+  $urlRouterProvider.otherwise('/app/search');
 });
